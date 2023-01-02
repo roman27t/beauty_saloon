05e2858d8819e3fdc7d561496fbb9187df817518
roman <roman.teterin.01@privatbank.ua>
2023-01-02 11:53:20 +0200

    style: test_index

--- app/tests/test_index.py
+++ app/tests/test_index.py
@@ -15,4 +15,4 @@ async def test_index(async_client: AsyncClient, async_session: AsyncSession):
 async def test_1(async_client: AsyncClient, async_session: AsyncSession):
     response = await async_client.get(url_reverse('view_read_item', item_id=1))
     assert response.status_code == 200
-    assert response.json() == {"item_id": 1, "q": None}
+    assert response.json() == {'item_id': 1, 'q': None}
