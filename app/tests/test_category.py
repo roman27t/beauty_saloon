4675e1d74db85cdfd0d128e05312747bbdc36cfb
roman <teterin.roman.27@gmail.com>
2023-01-05 20:22:48 +0200

    add: test_post_category

--- app/tests/test_category.py
+++ app/tests/test_category.py
@@ -2,8 +2,10 @@ import pytest
 from httpx import AsyncClient
 from fastapi import status
 from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy import select
 
 from models import CategoryModel
+from models.service_model import CategoryInSchema
 from tests.utils import url_reverse
 from services.stub_init_service import CATEGORIES
 
@@ -27,3 +29,26 @@ async def test_get_category_by_id(
     if status_code == status.HTTP_200_OK:
         category = CategoryModel(**response.json())
         assert category.name == CATEGORIES[0]
+
+
+@pytest.mark.parametrize(
+    'is_error, status_code',
+    [
+        (False, status.HTTP_200_OK),
+        (True, status.HTTP_422_UNPROCESSABLE_ENTITY),
+    ],
+)
+@pytest.mark.asyncio
+async def test_post_category(
+    async_client: AsyncClient, async_session: AsyncSession, is_error: bool, status_code: int
+):
+    schema = CategoryInSchema(**{'name': 'test_category', 'detail': 'detail'})
+    content = '{}' if is_error else schema.json()
+    response = await async_client.post(url_reverse('view_add_category'), content=content)
+    assert response.status_code == status_code
+    if status_code == status.HTTP_200_OK:
+        result = await async_session.execute(
+            select(CategoryModel).where(CategoryModel.name == schema.name).limit(1)
+        )
+        user_db = result.scalars().first()
+        assert user_db.name == schema.name
