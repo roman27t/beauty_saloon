7ee182ffe5b26d31ae2a32f2fda69fe1d40d40e7
roman <teterin.roman.27@gmail.com>
2023-01-06 21:28:26 +0200

    update: admin - fileds

--- app/admin/service_admin.py
+++ app/admin/service_admin.py
@@ -4,8 +4,23 @@ from models import CategoryModel, ServiceNameModel
 
 
 class ServiceCategoryAdmin(ModelView, model=CategoryModel):
-    column_list = [CategoryModel.id, CategoryModel.name, CategoryModel.detail]
+    column_list = [
+        CategoryModel.id,
+        CategoryModel.created_at,
+        CategoryModel.changed_at,
+        CategoryModel.name,
+        CategoryModel.detail,
+    ]
+    column_searchable_list = [CategoryModel.name]
 
 
 class ServiceNameAdmin(ModelView, model=ServiceNameModel):
-    column_list = [ServiceNameModel.id, ServiceNameModel.name, ServiceNameModel.detail]
+    column_list = [
+        ServiceNameModel.id,
+        ServiceNameModel.created_at,
+        ServiceNameModel.changed_at,
+        ServiceNameModel.name,
+        ServiceNameModel.is_active,
+        ServiceNameModel.price,
+    ]
+    column_searchable_list = [ServiceNameModel.name]
