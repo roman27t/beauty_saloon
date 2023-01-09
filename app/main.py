c9ddf3ca94b1b7ecd33c8af295243eca24d9ce56
roman <teterin.roman.27@gmail.com>
2023-01-09 19:03:16 +0200

    move: routers_all (2)

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from sqladmin import Admin
 
 from admin import admin_classes
 from routers import routers_all
-from routers.index import router_index
 from models.database import engine
 
 app = FastAPI()
@@ -12,7 +11,6 @@ admin = Admin(app, engine)
 for admin_class in admin_classes:
     admin.add_view(admin_class)
 
-app.include_router(router_index)
 for route in routers_all:
     app.include_router(route)  # , prefix='/api/auth', tags=['auth']
 
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -4,16 +4,16 @@ from routers.offer_routers import offer_service
 from routers.client_routers import router_client
 from routers.service_routers import router_service
 from routers.category_routers import router_category
-from routers.employee_routers import router
+from routers.employee_routers import router_employee
 from routers.stub_init_routers import router_init_stub
 
 routers_all = [
     router_index,
-    router,
+    router_employee,
+    router_client,
+    router_category,
     router_service,
     offer_service,
-    router_category,
-    router_client,
 ]
 if i_config.DEBUG:
     routers_all.append(router_init_stub)
