3f61cd3b05a017ddc7e82c9f15bacec4d17ae18d
roman <teterin.roman.27@gmail.com>
2023-01-21 17:03:23 +0200

    update: view_delete_employee

--- app/routers/employee_routers.py
+++ app/routers/employee_routers.py
@@ -1,12 +1,12 @@
 from fastapi import Depends, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from dependencies.base_dependency import ValidGetByIdDependency, valid_empty_schema
 from models import EmployeeModel
 from routers.consts import RouteSlug
 from models.database import get_session
 from schemas.user_schemas import EmployeeInSchema, EmployeeInOptionalSchema
 from services.employee_service import EmployeeService
-from dependencies.employee_dependency import valid_patch_id, valid_patch_schema
 
 router_employee = APIRouter()
 ROUTE_EMPLOYEE = '/employee/'
@@ -29,8 +29,8 @@ async def view_add_employee(employee: EmployeeInSchema, session: AsyncSession = 
 
 @router_employee.patch(ROUTE_EMPLOYEE + RouteSlug.pk, response_model=EmployeeModel)
 async def view_patch_employee(
-    schema: EmployeeInOptionalSchema = Depends(valid_patch_schema),
-    employee_db: EmployeeModel = Depends(valid_patch_id),
+    schema: EmployeeInOptionalSchema = Depends(valid_empty_schema(class_schema=EmployeeInOptionalSchema)),
+    employee_db: EmployeeModel = Depends(ValidGetByIdDependency(model=EmployeeModel)),
     session: AsyncSession = Depends(get_session),
 ):
     await EmployeeService(db_session=session).update(obj_db=employee_db, schema=schema)
--- app/services/employee_service.py
+++ app/services/employee_service.py
@@ -1,6 +1,7 @@
 from typing import Type
 
 from models import EmployeeModel
+from services import ServiceRegistry
 from services.base_service import AbstractService
 
 
@@ -8,3 +9,6 @@ class EmployeeService(AbstractService):
     @property
     def _table(self) -> Type[EmployeeModel]:
         return EmployeeModel
+
+
+ServiceRegistry.register(model=EmployeeModel, service_class=EmployeeService)
