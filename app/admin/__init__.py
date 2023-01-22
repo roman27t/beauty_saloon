31db8192269597831002c86327fb63ed40c081c4
roman <teterin.roman.27@gmail.com>
2023-01-22 15:59:48 +0200

    move: User - admin

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,11 +1,8 @@
-from .user_admin import ClientAdmin, EmployeeAdmin
 from .offer_admin import OfferLinkAdmin
 from .order_admin import OrderAdmin, OrderDetailAdmin
 from .service_admin import ServiceNameAdmin
 
 admin_classes = (
-    EmployeeAdmin,
-    ClientAdmin,
     ServiceNameAdmin,
     OfferLinkAdmin,
     OrderAdmin,
--- app/includer/admins.py
+++ app/includer/admins.py
@@ -1,6 +1,8 @@
 from entities.category.admins_category import ServiceCategoryAdmin
-
+from entities.users.admins_user import EmployeeAdmin, ClientAdmin
 
 admin_classes2 = (
     ServiceCategoryAdmin,
+    EmployeeAdmin,
+    ClientAdmin,
 )
