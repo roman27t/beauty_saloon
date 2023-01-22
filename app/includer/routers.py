3e12dc66f98ae20149d5604b85b052491266da74
roman <teterin.roman.27@gmail.com>
2023-01-22 16:07:25 +0200

    move: User - routers

--- app/includer/routers.py
+++ app/includer/routers.py
@@ -1,5 +1,9 @@
 from entities.category.routers_category import router_category
+from entities.users.routers.client_routers import router_client
+from entities.users.routers.employee_routers import router_employee
 
 routers_all2 = [
     router_category,
+    router_client,
+    router_employee,
 ]
--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -2,15 +2,11 @@ from config import i_config
 from routers.index import router_index
 from routers.offer_routers import router_offer
 from routers.order_routers import router_order
-from routers.client_routers import router_client
 from routers.service_routers import router_service
-from routers.employee_routers import router_employee
 from routers.stub_init_routers import router_init_stub
 
 routers_all = [
     router_index,
-    router_employee,
-    router_client,
     router_service,
     router_offer,
     router_order,
