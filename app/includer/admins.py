3de1dea7e14eaf9fb12414fe9efce50b87c84071
roman <teterin.roman.27@gmail.com>
2023-01-22 17:09:58 +0200

    rename: order - admins

--- app/includer/admins.py
+++ app/includer/admins.py
@@ -1,6 +1,6 @@
 from entities.category.admins_category import CategoryAdmin
 from entities.offer.admins_offer import OfferLinkAdmin
-from entities.order.order_admin import OrderAdmin, OrderDetailAdmin
+from entities.order.admins_order import OrderAdmin, OrderDetailAdmin
 from entities.service_name.admins_service_name import ServiceNameAdmin
 from entities.users.admins_user import EmployeeAdmin, ClientAdmin
 
