ac47a81f6504537f2d0cac9fc93e3e585051aa2b
roman27t <1290763+roman27t@users.noreply.github.com>
2023-02-14 16:05:10 +0200

    fix: swagger - ValidOrderByDependency (#28)

--- app/tests/test_index.py
+++ app/tests/test_index.py
@@ -1,13 +1,19 @@
 import pytest
 from httpx import AsyncClient
 from fastapi import status
-from sqlmodel.ext.asyncio.session import AsyncSession
 
 from tests.utils import url_reverse
 
 
 @pytest.mark.asyncio
-async def test_index(async_client: AsyncClient, async_session: AsyncSession):
+async def test_index(async_client: AsyncClient):
     response = await async_client.get(url_reverse('view_index'))
     assert response.status_code == status.HTTP_200_OK
     assert response.json() == {'ok': True}
+
+
+@pytest.mark.asyncio
+async def test_swagger_openapi(async_client: AsyncClient):
+    response = await async_client.get('/openapi.json')
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json()
